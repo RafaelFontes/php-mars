d5378f6de75db86e1e90549754e5ae40bc950a2a
Rafael <poste9@gmail.com>
2017-08-10 21:16:16 -0300

    added action interface

--- src/Action/AbstractAction.php
+++ src/Action/AbstractAction.php
@@ -1,8 +1,26 @@
 <?php
 /**
- * Copyright (C) 2017 Rafael Fontes. All Rights Reserved.
+ * File: src/Middleware/ActionLoggerMiddleware.php
  *
- * Use of this source is governed by the LICENSE file found at https://github.com/rafaelfontes/php-mars
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to the license Unlicense that is available
+ * through the world-wide-web at the following URI:
+ * https://choosealicense.com/licenses/unlicense
+ *
+ * @author   Rafael Fontes <rafael.fontes@gmx.com>
+ * @license  https://choosealicense.com/licenses/unlicense Unlicense
+ * @link     https://github.com/rafaelfontes/php-mars
  */
 
+namespace RFontes\PHPMars\Action;
 
+use RFontes\Action\ActionInterface;
+
+abstract class AbstractAction implements ActionInterface
+{
+    public function getType()
+    {
+        return get_class($this);
+    }
+}
